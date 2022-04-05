ad993e4276819f1c55927cc11f92d6c54757c9e0
Gift Nnko <nnkogift@hotmail.com>
2022-04-05 17:03:31 +0300

    fix(build): Fixes - Fixed issues with build

--- packages/lib/src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import {helloWorld} from "./index";
-
-
-describe("Hello world test", () => {
-    it("should say hello world", () => {
-        expect(helloWorld()).toBe("Hello World!");
-    });
-})
