7d583b7c5c06edf5a1658722916deeea56d9cfeb
Gift Nnko <nnkogift@hotmail.com>
2022-04-06 16:23:31 +0300

    :sparkles: feat(interfaces): Implementation
    - Implemented interfaces for Tei,events,enrollments,user,orgUnit, program,

--- packages/lib/src/interfaces/dhis2.ts
+++ packages/lib/src/interfaces/dhis2.ts
@@ -14,7 +14,7 @@ export interface DHIS2Resource {
 }
 
 //TODO: Add more acceptable types
-export type DHIS2AccessString = "rw------" | "r-------" | "-------"
+export type DHIS2AccessString = "rw------" | "r-------" | "-------" | "rwrw----"
 
 //TODO: Copy from react library
 export type DHIS2ValueType = "LONG_TEXT" | ""
@@ -40,11 +40,15 @@ export interface DHIS2Sharing {
     };
 }
 
-export interface Legend {
+export interface Legend extends DHIS2Resource {
+    startValue: number;
+    endValue: number;
+    color: string;
+
 
 }
 
-export interface LegendSet {
+export interface LegendSet extends DHIS2Resource {
     id: string;
     name: string;
     legends: Legend[];
@@ -68,36 +72,103 @@ export interface TrackedEntityInstance extends DHIS2Resource {
 
 }
 
+export interface TrackedEntityType extends DHIS2Resource {
+}
+
+export interface DataValue {
+    value: string;
+    dataElement: string;
+}
+
 export interface Event extends DHIS2Resource {
+    event: string;
+    program: string;
+    programStage: string;
+    orgUnit: string;
+    orgUnitName: string;
+    trackedEntityInstance?: string;
+    eventDate: string;
+    status: string;
+    dataValues: DataValue[];
+    enrollmentStatus: string;
+    enrollment: string;
+
+
 }
 
 export interface DataElement extends DHIS2Resource {
+    valueType: DHIS2ValueType;
+    optionSet?: OptionSet;
+    legendSets?: LegendSet[];
+    dataElementGroups?: { id: string; }[];
 }
 
 export interface DataElementGroup extends DHIS2Resource {
+    dataElements?: { id: string; }[];
 }
 
 export interface OrganisationUnit extends DHIS2Resource {
+    path?: string;
+    level?: number;
+    organisationUnitGroups?: OrganisationUnitGroup[];
+    children: OrganisationUnit[];
+    parent?: OrganisationUnit;
+    ancestors?: OrganisationUnit[];
+
 }
 
 export interface OrganisationUnitGroup extends DHIS2Resource {
+    organisationUnits: OrganisationUnit[];
 }
 
 export interface OrganisationUnitLevel extends DHIS2Resource {
+    level?: number;
 }
 
 export interface Program extends DHIS2Resource {
+    trackedEntityType?: TrackedEntityType;
+    programType?: string;
+    programStages?: ProgramStage[];
+    programTrackedEntityAttributes?: {
+        trackedEntityAttribute: TrackedEntityAttribute;
+        program: Program
+
+    };
 }
 
 export interface ProgramRule extends DHIS2Resource {
 }
 
+
+export interface ProgramStageSection {
+
+}
+
+export interface ProgramStageDataElements {
+    dataElement: DataElement;
+    compulsory?: boolean;
+    allowProvidedElsewhere?: boolean;
+    displayInReports?: boolean;
+    sortOrder?: number;
+    programStageSection: ProgramStageSection;
+    programStage?: ProgramStage;
+}
+
 export interface ProgramStage extends DHIS2Resource {
+    program?: Program;
+    programStageDataElements?: ProgramStageDataElements[];
+    programStageSections?: ProgramStageSection[];
+    repeatable?: boolean;
 }
 
 export interface Enrollment extends DHIS2Resource {
+    program?: Program;
+    events?: Event[];
+    enrollment?: string;
+    trackedEntityInstance?: string;
+    orgUnit?: string;
+    enrollmentDate?: string;
 }
-
 
 export interface DHIS2UserGroup extends DHIS2Resource {
     externalAccess?: boolean;
@@ -147,5 +218,9 @@ export interface Icon extends DHIS2Resource {
 }
 
 export interface DataSet extends DHIS2Resource {
+
+}
+
+export interface Analytics {
 
 }
