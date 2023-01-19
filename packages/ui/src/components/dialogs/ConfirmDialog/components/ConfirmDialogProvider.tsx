0cdbdf3d28816fc97e992cd13ed5c8e121dcd49e
gnnko <nnkogift@hotmail.com>
2023-01-19 09:37:07 +0300

    feat(dialogs): Implementation
    - Implemented confirm dialog

---
+++ packages/ui/src/components/dialogs/ConfirmDialog/components/ConfirmDialogProvider.tsx
@@ -0,0 +1,58 @@
+import React, {ReactNode, useCallback, useState} from "react";
+import {ConfirmDialogContext} from "../states/dialog";
+import ConfirmDialog, {CustomAction} from "./ConfirmDialog";
+
+export interface ConfirmDialogConfig {
+  title: string;
+  message: string | ReactNode;
+  onConfirm: () => void;
+  onCancel: () => void;
+  size?: "small" | "large";
+  position?: "top" | "bottom" | "middle";
+  cancelButtonText?: string;
+  confirmButtonText?: string;
+  confirmButtonColor?: "primary" | "secondary" | "destructive";
+  customActions?: CustomAction[];
+}
+
+export default function ConfirmDialogProvider({ children }: { children: ReactNode }) {
+  const [open, setOpen] = useState(false);
+  const [config, setConfig] = useState<ConfirmDialogConfig | null>();
+
+  const confirm = useCallback((config: ConfirmDialogConfig) => {
+    setConfig(config);
+    setOpen(true);
+  }, []);
+
+  const onCancel = useCallback(() => {
+    setOpen(false);
+    setConfig(null);
+    if (config?.onCancel) {
+      config.onCancel();
+    }
+  }, [config]);
+
+  const onConfirm = useCallback(() => {
+    setOpen(false);
+    setConfig(null);
+    if (config) {
+      config.onConfirm();
+    }
+  }, [config]);
+
+  const customActions = config?.customActions?.map((actions) => ({
+    ...actions,
+    onClick: () => {
+      setOpen(false);
+      setConfig(null);
+      actions.onClick();
+    },
+  }));
+
+  return (
+    <ConfirmDialogContext.Provider value={{ confirm }}>
+      {children}
+      {config && <ConfirmDialog hide={!open} {...config} customActions={customActions} onConfirm={onConfirm} onCancel={onCancel} />}
+    </ConfirmDialogContext.Provider>
+  );
+}
---
+++ packages/ui/src/components/dialogs/ConfirmDialog/states/dialog.ts
@@ -0,0 +1,8 @@
+import {createContext} from "react";
+import {ConfirmDialogConfig} from "../components/ConfirmDialogProvider";
+
+export const ConfirmDialogContext = createContext<{ confirm: (config: ConfirmDialogConfig) => void }>({
+  confirm: () => {
+    return;
+  },
+});
