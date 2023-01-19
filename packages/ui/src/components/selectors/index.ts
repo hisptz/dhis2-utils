0f6dca7e72c020bf8220467f37149d553e248649
gnnko <nnkogift@hotmail.com>
2023-01-19 18:54:18 +0300

    chore(custom data table, selectors): Chore
    - Exported all components

---
+++ packages/ui/src/components/selectors/index.ts
@@ -0,0 +1,4 @@
+export * from "./DataSourceSelector"
+export * from "./Modals"
+export * from "./OrgUnitSelector"
+export * from "./PeriodSelector"
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,4 @@
 export * from "./components/forms"
+export * from "./components/dialogs";
+export * from "./components/tables";
+export * from "./components/selectors";
