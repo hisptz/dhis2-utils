a452b0cc008b5847a66e131e391ec814ab1b9d12
gnnko <nnkogift@hotmail.com>
2023-01-19 09:48:12 +0300

    feat(dialogs): Implementation
    - Added async support for confirm dialog

--- packages/ui/src/components/dialogs/ConfirmDialog/components/ConfirmDialog/index.tsx
+++ packages/ui/src/components/dialogs/ConfirmDialog/components/ConfirmDialog/index.tsx
@@ -3,68 +3,73 @@ import {Button, ButtonStrip, Modal, ModalActions, ModalContent, ModalTitle} from
 import React, {ReactNode} from "react";
 
 export interface CustomAction {
-  label: string;
-  onClick: () => void;
-  properties?: string[];
-  color?: "primary" | "secondary" | "destructive";
+    label: string;
+    onClick: () => void;
+    properties?: string[];
+    color?: "primary" | "secondary" | "destructive";
 }
 
 export interface ConfirmDialogProps {
-  title: string;
-  message: string | ReactNode;
-  size?: "small" | "large";
-  position?: "top" | "bottom" | "middle";
-  onConfirm: () => void;
-  onCancel?: () => void;
-  cancelButtonText?: string;
-  confirmButtonText?: string;
-  hide: boolean;
-  confirmButtonColor?: "primary" | "secondary" | "destructive";
-  customActions?: CustomAction[];
+    title: string;
+    message: string | ReactNode;
+    size?: "small" | "large";
+    position?: "top" | "bottom" | "middle";
+    onConfirm: () => void;
+    onCancel?: () => void;
+    cancelButtonText?: string;
+    confirmButtonText?: string;
+    hide: boolean;
+    confirmButtonColor?: "primary" | "secondary" | "destructive";
+    customActions?: CustomAction[];
+    loading?: boolean;
+    loadingText?: string;
 }
 
 export default function ConfirmDialog({
-                                        message,
-                                        size,
-                                        onConfirm,
-                                        onCancel,
-                                        cancelButtonText,
-                                        confirmButtonText,
-                                        hide,
-                                        confirmButtonColor,
-                                        title,
-                                        position,
-                                        customActions
+                                          message,
+                                          loading,
+                                          size,
+                                          onConfirm,
+                                          onCancel,
+                                          cancelButtonText,
+                                          confirmButtonText,
+                                          hide,
+                                          confirmButtonColor,
+                                          title,
+                                          position,
+                                          customActions,
+                                          loadingText
                                       }: ConfirmDialogProps) {
-  return (
-    <Modal position={position ?? "middle"} hide={hide} small={size === "small" || size === undefined}
-           large={size === "large"}>
-      <ModalTitle>{title ?? i18n.t("Confirm Action")}</ModalTitle>
-      <ModalContent>{message}</ModalContent>
-      <ModalActions>
-        <ButtonStrip>
-          <Button onClick={onCancel}>{cancelButtonText ?? i18n.t("Cancel")}</Button>
-          {customActions &&
-            customActions.map(({ label, color, onClick, properties }, index) => (
-              <Button
-                key={`${label}-${index}`}
-                primary={color === "primary"}
-                secondary={color === "secondary" || color === undefined}
-                destructive={color === "destructive"}
-                onClick={onClick}
-                {...properties}>
-                {label}
-              </Button>
-            ))}
-          <Button
-            primary={confirmButtonColor === "primary"}
-            secondary={confirmButtonColor === "secondary"}
-            destructive={confirmButtonColor === "destructive" || confirmButtonColor === undefined}
-            onClick={onConfirm}>
-            {confirmButtonText ?? i18n.t("Confirm")}
-          </Button>
-        </ButtonStrip>
-      </ModalActions>
-    </Modal>
-  );
+    return (
+        <Modal position={position ?? "middle"} hide={hide} small={size === "small" || size === undefined}
+               large={size === "large"}>
+            <ModalTitle>{title ?? i18n.t("Confirm Action")}</ModalTitle>
+            <ModalContent>{message}</ModalContent>
+            <ModalActions>
+                <ButtonStrip>
+                    <Button onClick={onCancel}>{cancelButtonText ?? i18n.t("Cancel")}</Button>
+                    {customActions &&
+                        customActions.map(({label, color, onClick, properties}, index) => (
+                            <Button
+                                key={`${label}-${index}`}
+                                primary={color === "primary"}
+                                secondary={color === "secondary" || color === undefined}
+                                destructive={color === "destructive"}
+                                onClick={onClick}
+                                {...properties}>
+                                {label}
+                            </Button>
+                        ))}
+                    <Button
+                        loading={loading}
+                        primary={confirmButtonColor === "primary"}
+                        secondary={confirmButtonColor === "secondary"}
+                        destructive={confirmButtonColor === "destructive" || confirmButtonColor === undefined}
+                        onClick={onConfirm}>
+                        {loading ? (loadingText ?? i18n.t("Confirming...")) : (confirmButtonText ?? i18n.t("Confirm"))}
+                    </Button>
+                </ButtonStrip>
+            </ModalActions>
+        </Modal>
+    );
 }
