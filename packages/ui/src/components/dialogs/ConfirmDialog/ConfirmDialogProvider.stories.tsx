159cd925ea89d481d26e50dfcf67d625ed133c49
gnnko <nnkogift@hotmail.com>
2023-01-26 11:14:56 +0300

    fix(confirm dialog): Fixes
    - Fixed issues with async confirm dialog not working
     Fixes #36

--- packages/ui/src/components/dialogs/ConfirmDialog/ConfirmDialogProvider.stories.tsx
+++ packages/ui/src/components/dialogs/ConfirmDialog/ConfirmDialogProvider.stories.tsx
@@ -3,74 +3,89 @@ import type {Story} from "@storybook/react";
 import React, {ReactNode} from "react";
 import {ConfirmDialogConfig, ConfirmDialogProvider, useConfirmDialog} from "./index";
 
-const ExampleChild = ({ config }: { config?: ConfirmDialogConfig }) => {
-  const { confirm } = useConfirmDialog();
+const ExampleChild = ({config, isAsync}: { config?: ConfirmDialogConfig, isAsync?: boolean }) => {
+    const {confirm} = useConfirmDialog();
 
-  return (
-    <Button
-      onClick={() => {
-        confirm(
-          config ?? {
-            title: "Are you sure?",
-            message: "Your confirm message will appear here",
-            onConfirm: () => {
-              alert("Confirmed 🤗");
-            },
-            onCancel: () => {
-              alert("Cancelled 😔");
-            },
-          }
-        );
-      }}>
-      Click me!
-    </Button>
-  );
+    return (
+        <Button
+            onClick={() => {
+                confirm(
+                    config ?? {
+                        title: "Are you sure?",
+                        message: "Your confirm message will appear here",
+                        onConfirm: isAsync ? async () => {
+                            alert(await new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    resolve("Confirmed 🤗")
+                                }, 5000)
+                            }))
+                        } : () => {
+                            alert("Confirmed 🤗");
+                        },
+                        onCancel: () => {
+                            alert("Cancelled 😔");
+                        },
+                    }
+                );
+            }}>
+            Click me!
+        </Button>
+    );
 };
 
-const Template: Story<{ children: ReactNode }> = (args) => <ConfirmDialogProvider>{args.children}</ConfirmDialogProvider>;
+const Template: Story<{ children: ReactNode }> = (args) =>
+    <ConfirmDialogProvider>{args.children}</ConfirmDialogProvider>;
 
 export const ConfirmDialogInProvider = Template.bind({});
 ConfirmDialogInProvider.args = {
-  children: (
-    <div>
-      <ExampleChild />
-    </div>
-  ),
+    children: (
+        <div>
+            <ExampleChild/>
+        </div>
+    ),
+};
+export const ConfirmDialogInProviderWithAsync = Template.bind({});
+ConfirmDialogInProviderWithAsync.args = {
+    children: (
+        <div>
+            <ExampleChild isAsync/>
+        </div>
+    ),
 };
 
 export const ConfirmDialogInProviderWithOptions = Template.bind({});
 ConfirmDialogInProviderWithOptions.args = {
-  children: (
-    <div>
-      <ExampleChild
-        config={{
-          title: "Are you sure?",
-          message: "Your confirm message will appear here",
-          onConfirm: () => {
-            alert("Confirmed 🤗");
-          },
-          onCancel: () => {
-            alert("Cancelled 😔");
-          },
-          customActions: [
-            {
-              label: "Custom",
-              color: "secondary",
-              onClick: () => {
-                alert("Custom 😉");
-              },
-            },
-          ],
-          cancelButtonText: "Custom cancel",
-          confirmButtonText: "Custom confirm",
-          confirmButtonColor: "primary",
-        }}
-      />
-    </div>
-  ),
+    children: (
+        <div>
+            <ExampleChild
+                config={{
+                    title: "Are you sure?",
+                    message: "Your confirm message will appear here",
+                    onConfirm: () => {
+                        alert("Confirmed 🤗");
+                    },
+                    onCancel: () => {
+                        alert("Cancelled 😔");
+                    },
+                    customActions: [
+                        {
+                            label: "Custom",
+                            color: "secondary",
+                            onClick: () => {
+                                alert("Custom 😉");
+                            },
+                        },
+                    ],
+                    cancelButtonText: "Custom cancel",
+                    confirmButtonText: "Custom confirm",
+                    confirmButtonColor: "primary",
+                }}
+            />
+        </div>
+    ),
 };
 
 export default {
-  title: "Dialogs/Confirm Dialog/Confirm Dialog Provider",
-  component: ConfirmDialogInProvider,
+    title: "Dialogs/Confirm Dialog/Confirm Dialog Provider",
+    component: ConfirmDialogInProviderWithAsync,
 };
