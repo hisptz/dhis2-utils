b5ecb2d577d802fd1e7ef410f5443b13bdd8265f
gnnko <nnkogift@hotmail.com>
2023-02-10 12:58:04 +0300

    feat(utils): Program rules implementation
    - Implemented program rules evaluation utilities

---
+++ packages/lib/src/utils/programRules/index.ts
@@ -0,0 +1,3 @@
+export * from "./utils/evaluator"
+export * from "./constants"
+export * from "./interfaces"
---
+++ packages/lib/src/utils/programRules/utils/variables.ts
@@ -0,0 +1,53 @@
+import {filter, find, head, last, sortBy} from 'lodash';
+import {MetaValues} from "../interfaces";
+import {Enrollment, Event} from "../../../interfaces";
+
+export function getPreviousEvent(meta: MetaValues): Event | undefined {
+    const {event, trackedEntityInstance, program} = meta;
+    const enrollment = find(trackedEntityInstance?.enrollments, ['program', program.id]);
+    if (!enrollment) {
+        return undefined;
+    }
+
+    const sortedEvents = sortBy(
+        filter(enrollment.events, (e) => e.event !== event?.event),
+        ['created', 'eventDate']
+    );
+
+    return last(
+        sortedEvents.filter(
+            (e) =>
+                new Date(e.eventDate) < new Date(event?.created) ||
+                new Date(e?.eventDate) < new Date(event?.eventDate ?? '')
+        )
+    );
+}
+
+export function getNewestProgramStageEvent({
+                                               trackedEntityInstance,
+                                               program,
+                                               programStage,
+                                           }: MetaValues): Event | undefined {
+    const enrollment = trackedEntityInstance?.enrollments?.find(
+        (enrollment: Enrollment) => enrollment?.program === program.id
+    );
+    const programStageEvents = enrollment?.events?.filter(
+        (event: Event) => programStage === event?.programStage
+    );
+    const sortedEvents = sortBy(
+        programStageEvents,
+        (event: Event) => new Date(event?.eventDate)
+    );
+    return head(sortedEvents);
+}
+
+export function getNewestProgramEvent(metaValues: MetaValues): Event | undefined {
+    const enrollment = metaValues.trackedEntityInstance?.enrollments?.find(
+        (enrollment: Enrollment) => enrollment?.program === metaValues.program.id
+    );
+    const sortedEvents = sortBy(
+        enrollment?.events,
+        (eventDate: Event) => new Date(eventDate?.eventDate)
+    );
+    return head(sortedEvents);
+}
