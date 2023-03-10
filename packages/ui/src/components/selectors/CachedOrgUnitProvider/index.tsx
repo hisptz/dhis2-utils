8c562e053f877ba6673e07a178206f21096d91bf
gnnko <nnkogift@hotmail.com>
2023-03-10 14:11:49 +0300

    fix(period selector): Fixes
    - Added control of period input type to allow date range as a  standalone

--- packages/ui/src/components/selectors/CachedOrgUnitProvider/index.tsx
+++ packages/ui/src/components/selectors/CachedOrgUnitProvider/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import CustomOrgUnitProvider from "./components/CustomOrgUnitProvider";
+import {CustomOrgUnitProvider} from "./components/CustomOrgUnitProvider";
 import {useClearOrganisationData, useOrganisationUnitData} from "./hooks";
 import {db} from "./services/db";
 
 interface OfflineOrgUnitProviderProps {
-  children: React.ReactNode;
-  fallback?: React.ReactNode;
-  pageSize?: number;
+    children: React.ReactNode;
+    fallback?: React.ReactNode;
+    pageSize?: number;
 }
 
-function OfflineOrgUnitProvider({ children, fallback, pageSize }: OfflineOrgUnitProviderProps) {
-  const { loading } = useOrganisationUnitData(pageSize);
+function OfflineOrgUnitProvider({children, fallback, pageSize}: OfflineOrgUnitProviderProps) {
+    const {loading} = useOrganisationUnitData(pageSize);
 
-  if (fallback && loading) {
-    return <>{fallback}</>;
-  }
+    if (fallback && loading) {
+        return <>{fallback}</>;
+    }
 
-  return <>{children}</>;
+    return <>{children}</>;
 }
 
-export { useOrganisationUnitData, useClearOrganisationData, db, CustomOrgUnitProvider, OfflineOrgUnitProvider };
-export type { OfflineOrgUnitProviderProps };
+export {useOrganisationUnitData, useClearOrganisationData, db, CustomOrgUnitProvider, OfflineOrgUnitProvider};
+export type {OfflineOrgUnitProviderProps};
--- packages/ui/src/components/selectors/PeriodSelector/index.tsx
+++ packages/ui/src/components/selectors/PeriodSelector/index.tsx
@@ -10,68 +10,80 @@ export * from "./components/PeriodSelect"
 export * from "./components/FixedPeriodSelector"
 
 export function PeriodSelector({
-  excludedPeriodTypes,
-  selectedPeriods,
-  onSelect,
-  excludeFixedPeriods,
-  excludeRelativePeriods,
-  singleSelection,
-  enableDateRange,
-  defaultInputType,
-}: PeriodSelectorProps) {
-  const initialInputType = useMemo(() => {
-    if (!isEmpty(selectedPeriods)) {
-      const period: any = head(selectedPeriods ?? []);
-      if (period?.type === "RANGE") {
-        return "dateRange";
-      } else {
-        return "period";
-      }
-    } else {
-      return defaultInputType ?? "period";
-    }
-  }, []);
-  const [inputType, setInputType] = useState<string>(initialInputType);
+                                   excludedPeriodTypes,
+                                   selectedPeriods,
+                                   onSelect,
+                                   excludeFixedPeriods,
+                                   excludeRelativePeriods,
+                                   singleSelection,
+                                   enableDateRange,
+                                   defaultInputType,
+                                   enablePeriodSelector,
+                                   allowFuturePeriods
+                               }: PeriodSelectorProps) {
+    const initialInputType = useMemo(() => {
+        if (!isEmpty(selectedPeriods)) {
+            const period: any = head(selectedPeriods ?? []);
+            if (period?.type === "RANGE") {
+                if (enableDateRange) {
+                    return "dateRange";
+                } else {
+                    throw Error("Date range periods must be enabled by passing the prop `enableDateRange`")
+                }
+            } else {
+                if (enablePeriodSelector) {
+                    return "period"
+                } else {
+                    throw Error("Date range periods must be enabled by passing the prop `enablePeriodSelector`")
+                }
+            }
+        } else {
 
-  const onInputTypeChange = useCallback(
-    ({ value }: { value: string }) => {
-      onSelect({ items: [] });
-      setInputType(value);
-    },
-    [onSelect]
-  );
+            return defaultInputType ?? enableDateRange ? "dateRange" : "period";
+        }
+    }, []);
+    const [inputType, setInputType] = useState<string>(initialInputType);
 
-  return (
-    <div style={{display: "flex", flexDirection: "column", gap: 16}}>
-      {enableDateRange && (
-        <SegmentedControl
-          selected={inputType}
-          onChange={onInputTypeChange}
-          options={[
-            {
-              label: i18n.t("Period"),
-              value: "period",
-            },
-            {
-              label: i18n.t("Date Range"),
-              value: "dateRange",
-            },
-          ]}
-        />
-      )}
-      {inputType === "period" && (
-        <PeriodSelect
-          singleSelection={singleSelection}
-          excludedPeriodTypes={excludedPeriodTypes ?? []}
-          onSelect={onSelect as unknown as (selected: { items: string[] }) => void}
-          selectedPeriods={selectedPeriods as string[]}
-          excludeFixedPeriods={excludeFixedPeriods}
-          excludeRelativePeriods={excludeRelativePeriods}
-        />
-      )}
-      {inputType === "dateRange" && (
-        <DateRange value={selectedPeriods as DateRangeValue[]} onChange={onSelect as unknown as (selected: { items: DateRangeValue[] }) => void} />
-      )}
-    </div>
-  );
+    const onInputTypeChange = useCallback(
+        ({value}: { value: string }) => {
+            onSelect({items: []});
+            setInputType(value);
+        },
+        [onSelect]
+    );
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", gap: 16}}>
+            {(enableDateRange && enablePeriodSelector) && (
+                <SegmentedControl
+                    selected={inputType}
+                    onChange={onInputTypeChange}
+                    options={[
+                        {
+                            label: i18n.t("Period"),
+                            value: "period",
+                        },
+                        {
+                            label: i18n.t("Date Range"),
+                            value: "dateRange",
+                        },
+                    ]}
+                />
+            )}
+            {inputType === "period" && (
+                <PeriodSelect
+                    singleSelection={singleSelection}
+                    excludedPeriodTypes={excludedPeriodTypes ?? []}
+                    onSelect={onSelect as unknown as (selected: { items: string[] }) => void}
+                    selectedPeriods={selectedPeriods as string[]}
+                    excludeFixedPeriods={excludeFixedPeriods}
+                    excludeRelativePeriods={excludeRelativePeriods}
+                />
+            )}
+            {inputType === "dateRange" && (
+                <DateRange value={selectedPeriods as DateRangeValue[]}
+                           onChange={onSelect as unknown as (selected: { items: DateRangeValue[] }) => void}/>
+            )}
+        </div>
+    );
 }
