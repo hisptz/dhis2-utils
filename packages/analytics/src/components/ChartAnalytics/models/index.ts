c6c4c2f69f5cb8ab97f92f4e0a8862286eb4e430
gian <nnkogift@hotmail.com>
2023-06-02 08:19:18 +0300

    fix(charts): Fixes
    - Fixed issues with highcharts overrides
    
    Signed-off-by: gian <nnkogift@hotmail.com>

--- packages/analytics/src/components/ChartAnalytics/models/index.ts
+++ packages/analytics/src/components/ChartAnalytics/models/index.ts
@@ -3,76 +3,89 @@ import HighCharts from "highcharts";
 import {ChartConfig} from "../types/props";
 
 export abstract class DHIS2Chart {
-  id: string;
-  analytics: Analytics;
-  config: ChartConfig;
+    id: string;
+    analytics: Analytics;
+    config: ChartConfig;
 
-  constructor(id: string, analytics: Analytics, config: ChartConfig) {
-    this.id = id;
-    this.analytics = analytics;
-    this.config = config;
-  }
+    constructor(id: string, analytics: Analytics, config: ChartConfig) {
+        this.id = id;
+        this.analytics = analytics;
+        this.config = config;
+    }
 
-  abstract getHighchartsType(): string;
+    abstract getHighchartsType(): string;
 
-  getChartConfig(): HighCharts.ChartOptions & any {
-    return {
-      renderTo: this.id,
-      zoomType: "xy",
-      type: this.getHighchartsType(),
-      height: this.config?.height,
-      styledMode: false,
-    };
-  }
+    getChartConfig(): HighCharts.ChartOptions & any {
+        return {
+            renderTo: this.id,
+            zoomType: "xy",
+            type: this.getHighchartsType(),
+            height: this.config?.height,
+            styledMode: false,
+        };
+    }
 
-  getOptions(): HighCharts.Options {
-    return {
-      yAxis: this.getYAxis(),
-      chart: this.getChartConfig(),
-      colors: this.config?.colors ?? ["#2f7ed8", "#0d233a", "#8bbc21", "#910000", "#1aadce", "#492970", "#f28f43", "#77a1e5", "#c42525", "#a6c96a"],
-      series: this.getSeries(),
-      plotOptions: this.getPlotOptions(),
-      title: { text: "" },
-      xAxis: this.getXAxis(),
-      exporting: this.getExporting(),
-      legend: { enabled: true },
-      credits: { enabled: false },
-      ...(this.config?.highChartOverrides ?? {}),
-    };
-  }
+    getOptions(): HighCharts.Options {
 
-  abstract getSeries(): HighCharts.SeriesOptionsType[];
+        const options = {
+            yAxis: this.getYAxis(),
+            chart: this.getChartConfig(),
+            colors: this.config?.colors ?? ["#2f7ed8", "#0d233a", "#8bbc21", "#910000", "#1aadce", "#492970", "#f28f43", "#77a1e5", "#c42525", "#a6c96a"],
+            series: this.getSeries(),
+            plotOptions: this.getPlotOptions(),
+            title: {text: ""},
+            xAxis: this.getXAxis(),
+            exporting: this.getExporting(),
+            legend: {enabled: true},
+            credits: {enabled: false},
+        };
 
-  abstract getPlotOptions(): HighCharts.PlotOptions;
+        let overrides = {};
 
-  abstract getXAxis(): HighCharts.XAxisOptions | undefined;
+        if (this.config?.highChartOverrides) {
+            overrides = {
+                ...((typeof this.config?.highChartOverrides === "object" ? this.config?.highChartOverrides ?? {} : this.config?.highChartOverrides(options)))
+            }
+        }
 
-  getYAxis(): HighCharts.YAxisOptions[] {
-    return [
-      {
-        title: {
-          text: "",
-          style: { color: "#000000", fontWeight: "normal", fontSize: "14px" },
-        },
-        labels: { enabled: true, style: { color: "#000000", fontWeight: "normal", fontSize: "14px" } },
-        plotLines: [
-          { color: "#000000", dashStyle: "Solid", width: 2, zIndex: 1000, label: { text: "" } },
-          { color: "#bbbbbb", dashStyle: "Solid", zIndex: 1000, width: 2, label: { text: "" } },
-        ],
-      },
-    ];
-  }
+        return {
+            ...options,
+            ...overrides
+        }
+    }
 
-  getExporting(): HighCharts.ExportingOptions {
-    const name = this.config?.name ?? "chart";
-    return {
-      filename: `${name}`,
-      sourceWidth: 1200,
-      buttons: {
-        contextButton: {
-          enabled: false,
-        },
-      },
-    };
-  }
+    abstract getSeries(): HighCharts.SeriesOptionsType[];
+
+    abstract getPlotOptions(): HighCharts.PlotOptions;
+
+    abstract getXAxis(): HighCharts.XAxisOptions | undefined;
+
+    getYAxis(): HighCharts.YAxisOptions[] {
+        return [
+            {
+                title: {
+                    text: "",
+                    style: {color: "#000000", fontWeight: "normal", fontSize: "14px"},
+                },
+                labels: {enabled: true, style: {color: "#000000", fontWeight: "normal", fontSize: "14px"}},
+                plotLines: [
+                    {color: "#000000", dashStyle: "Solid", width: 2, zIndex: 1000, label: {text: ""}},
+                    {color: "#bbbbbb", dashStyle: "Solid", zIndex: 1000, width: 2, label: {text: ""}},
+                ],
+            },
+        ];
+    }
+
+    getExporting(): HighCharts.ExportingOptions {
+        const name = this.config?.name ?? "chart";
+        return {
+            filename: `${name}`,
+            sourceWidth: 1200,
+            buttons: {
+                contextButton: {
+                    enabled: false,
+                },
+            },
+        };
+    }
 }
--- packages/analytics/src/components/ChartAnalytics/types/props.tsx
+++ packages/analytics/src/components/ChartAnalytics/types/props.tsx
@@ -1,48 +1,48 @@
 import type {Analytics} from "@hisptz/dhis2-utils";
-import {DashStyleValue, YAxisOptions, YAxisPlotLinesLabelOptions} from "highcharts";
+import HighCharts, {DashStyleValue, YAxisOptions, YAxisPlotLinesLabelOptions} from "highcharts";
 
 export type ChartType = "column" | "pie" | "stacked-column" | "line" | "multi-series";
 
 export interface MultiSeriesConfig {
-  series?: Array<{
-    id: string;
-    as: "column" | "line";
-    cumulative?: boolean;
-    yAxis?: number;
-  }>;
-  yAxes?: Array<YAxisOptions>;
-  target?: TargetConfig;
+    series?: Array<{
+        id: string;
+        as: "column" | "line";
+        cumulative?: boolean;
+        yAxis?: number;
+    }>;
+    yAxes?: Array<YAxisOptions>;
+    target?: TargetConfig;
 }
 
 export interface TargetConfig {
-  id: string;
-  value: number;
-  label?: YAxisPlotLinesLabelOptions;
-  styles: {
-    color?: string;
-    width?: number;
-    dashStyle?: DashStyleValue;
-    zIndex?: number;
-  };
+    id: string;
+    value: number;
+    label?: YAxisPlotLinesLabelOptions;
+    styles: {
+        color?: string;
+        width?: number;
+        dashStyle?: DashStyleValue;
+        zIndex?: number;
+    };
 }
 
 export type ChartConfig = {
-  layout: {
-    series: Array<string>;
-    category: Array<string>;
-    filter: Array<string>;
-  };
-  type?: ChartType;
-  height?: number;
-  colors?: Array<string>;
-  name?: string;
-  allowChartTypeChange?: boolean;
-  highChartOverrides?: Record<string, any>;
-  multiSeries?: MultiSeriesConfig;
+    layout: {
+        series: Array<string>;
+        category: Array<string>;
+        filter: Array<string>;
+    };
+    type?: ChartType;
+    height?: number;
+    colors?: Array<string>;
+    name?: string;
+    allowChartTypeChange?: boolean;
+    highChartOverrides?: Partial<HighCharts.Options> | ((config: HighCharts.Options) => Partial<HighCharts.Options>);
+    multiSeries?: MultiSeriesConfig;
 };
 
 export type ChartAnalyticsProps = {
-  analytics: Analytics;
-  config: ChartConfig;
-  containerProps?: Record<string, any>;
+    analytics: Analytics;
+    config: ChartConfig;
+    containerProps?: Record<string, any>;
 };
