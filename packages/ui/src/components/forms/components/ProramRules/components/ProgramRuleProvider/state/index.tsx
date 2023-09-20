34ebd2ede9bf6ef48b65245e8438e3f4ef0f8c8a
Gift Nnko <nnkogift@hotmail.com>
2023-09-20 10:48:01 +0300

    fix(program rules): Fixes
    - Fixed issues with program rules provider staying in suspense
     - Improved field state selector

--- packages/ui/src/components/forms/components/ProramRules/components/ProgramRuleProvider/state/index.tsx
+++ packages/ui/src/components/forms/components/ProramRules/components/ProgramRuleProvider/state/index.tsx
@@ -3,58 +3,82 @@ import {atomFamily, RecoilRoot, selectorFamily} from "recoil";
 
 
 export const FieldVisibilityState = atomFamily({
-    key: 'field-visibility-state',
-    default: false,
+		key: 'field-visibility-state',
+		default: false,
 });
 
 export const FieldHiddenOptionsState = atomFamily<string[], string>({
-    key: 'field-hidden-options-state',
-    default: [],
+		key: 'field-hidden-options-state',
+		default: [],
 });
 
 export const FieldWarningState = atomFamily<string, string>({
-    key: 'field-warning-state',
-    default: '',
+		key: 'field-warning-state',
+		default: '',
 });
 
 export const FieldLoadingState = atomFamily<boolean, string>({
-    key: 'field-loading-state',
-    default: false,
+		key: 'field-loading-state',
+		default: false,
 });
 
 export const FieldDisabledState = atomFamily<boolean, string>({
-    key: 'field-disabled-state',
-    default: false,
+		key: 'field-disabled-state',
+		default: false,
 });
 
 export const FieldMinMaxState = atomFamily<
-    { min?: number | string; max?: number | string },
-    string
+		{ min?: number | string; max?: number | string } | null,
+		string
 >({
-    key: 'field-min-max-state',
-    default: undefined,
+		key: 'field-min-max-state',
+		default: null,
 });
-
-
 export const FieldState = selectorFamily({
-    key: 'field-state',
-    get: (field: string) => ({get}) => {
+		key: 'field-state',
+		get: (field: string) => ({get, getCallback}) => {
 
-        return {
-            hidden: get(FieldVisibilityState(field)),
-            hiddenOptions: get(FieldHiddenOptionsState(field)),
-            warning: get(FieldWarningState(field)),
-            disabled: get(FieldDisabledState(field)),
-            minMax: get(FieldMinMaxState(field)),
-            loading: get(FieldLoadingState(field))
-        }
-    }
+				const setFieldState = getCallback(({set}) => (type: 'hidden' | 'hiddenOptions' | 'warning' | 'disabled' | 'minMax' | 'loading', value: any) => {
+						switch (type) {
+								case 'hidden':
+										set(FieldVisibilityState(field), value)
+										break
+								case 'hiddenOptions':
+										set(FieldHiddenOptionsState(field), value)
+										break
+								case 'warning':
+										set(FieldWarningState(field), value)
+										break
+								case 'disabled':
+										set(FieldDisabledState(field), value)
+										break
+								case 'minMax':
+										set(FieldMinMaxState(field), value)
+										break
+								case 'loading':
+										set(FieldLoadingState(field), value)
+										break
+								default:
+										break
+						}
+				})
+
+				return {
+						hidden: get(FieldVisibilityState(field)),
+						hiddenOptions: get(FieldHiddenOptionsState(field)),
+						warning: get(FieldWarningState(field)),
+						disabled: get(FieldDisabledState(field)),
+						minMax: get(FieldMinMaxState(field)),
+						loading: get(FieldLoadingState(field)),
+						setFieldState: setFieldState
+				}
+		}
 })
 
 export const FieldStateProvider = React.memo(function FieldStateProvider({children}: { children: React.ReactNode }) {
-    return (
-        <RecoilRoot>
-            {children}
-        </RecoilRoot>
-    )
+		return (
+				<RecoilRoot>
+						{children}
+				</RecoilRoot>
+		)
 })
