3199a2eaeeb62a915dff312a6c3d39eab4c5c8b8
Gift Nnko <nnkogift@hotmail.com>
2024-01-10 19:22:02 +0300

    chore(storybook): Initial setup
    - Setup storybook theme
     - Setup storybook config for libs

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,52 @@
+import { dirname, join, resolve } from "path";
+import { StorybookConfig } from "@storybook/react-vite";
+
+function getAbsolutePath(value: string) {
+	return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+	stories: [
+		{
+			directory: resolve(__dirname, "../../../packages/ui/src"),
+			files: "**/*.stories.tsx",
+			titlePrefix: "UI",
+		},
+	],
+	addons: [
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-docs"),
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+
+	core: {},
+
+	async viteFinal(config: any, { configType }: any) {
+		// customize the Vite config here
+		return {
+			...config,
+			define: { "process.env": {} },
+			resolve: {
+				alias: [
+					{
+						find: "ui",
+						replacement: resolve(
+							__dirname,
+							"../../../packages/ui/",
+						),
+					},
+				],
+			},
+		};
+	},
+
+	docs: {
+		autodocs: tru,
+	},
+};
+
+export default config;
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,8 +1,14 @@
-import {defineConfig} from "tsup";
+import { defineConfig } from "tsup";
 
-export default defineConfig({
-		entry: {
-				".": "src/index.ts",
-				CustomDataTable: "src/CustomDataTable/index.ts"
-		}
-})
+export default defineConfig((options) => {
+	const devMode = options.watch === true;
+	return {
+		...options,
+		entry: ["src/index.ts", "src/"],
+		splitting: !devMode,
+		clean: true,
+		dts: !devMode,
+		format: ["esm", "cjs"],
+		bundle: !devMode,
+	};
+});
