1b507800441d92900a002b25e98e3cbcd0c3ebfc
Gift Nnko <nnkogift@gmail.com>
2024-07-25 15:12:45 +0300

    fix: legend min max input
    Removed the normalize functionality

--- packages/ui/src/forms/components/LegendMinMaxGroup/hooks/control.ts
+++ packages/ui/src/forms/components/LegendMinMaxGroup/hooks/control.ts
@@ -3,70 +3,6 @@ import { useCallback } from "react";
 import { LegendValue } from "../../LegendMinMax/index.js";
 import { findIndex, set } from "lodash";
 
-function normalizeValues(
-	legends: LegendValue[],
-	value: LegendValue,
-	{
-		highIsGood,
-		legendDefinitions,
-	}: { highIsGood: boolean; legendDefinitions: LegendDefinition[] },
-) {
-	const updatedLegends = [...legends];
-	const index = findIndex(updatedLegends, [
-		"legendDefinitionId",
-		value.legendDefinitionId,
-	]);
-	const nextIndex = index + 1;
-	const prevIndex = index - 1;
-
-	if (highIsGood) {
-		if (nextIndex >= updatedLegends.length) {
-			const legendDefinitionId = legendDefinitions[nextIndex]?.id;
-			if (legendDefinitionId) {
-				set(updatedLegends, [nextIndex], {
-					legendDefinitionId,
-					endValue: value.startValue,
-				});
-			}
-		} else {
-			set(updatedLegends, [nextIndex], {
-				...updatedLegends[nextIndex],
-				endValue: value.startValue,
-			});
-		}
-		if (prevIndex >= 0) {
-			set(updatedLegends, [prevIndex], {
-				...updatedLegends[prevIndex],
-				startValue: value.endValue,
-			});
-		}
-	} else {
-		if (nextIndex >= updatedLegends.length) {
-			const legendDefinitionId = legendDefinitions[nextIndex]?.id;
-			if (legendDefinitionId) {
-				set(updatedLegends, [nextIndex], {
-					legendDefinitionId,
-					startValue: value.endValue,
-				});
-			}
-		} else {
-			set(updatedLegends, [nextIndex], {
-				...updatedLegends[nextIndex],
-				startValue: value.endValue,
-			});
-		}
-
-		if (prevIndex >= 0) {
-			set(updatedLegends, [prevIndex], {
-				...updatedLegends[prevIndex],
-				endValue: value.startValue,
-			});
-		}
-	}
-
-	return updatedLegends;
-}
-
 export function useControlMinMaxFields({
 	legendDefinitions,
 	value,
@@ -90,12 +26,7 @@ export function useControlMinMaxFields({
 			} else {
 				set(updatedList, [legendIndex], legend);
 			}
-			onFieldChange(
-				normalizeValues(updatedList, legend, {
-					highIsGood,
-					legendDefinitions,
-				}),
-			);
+			onFieldChange(updatedList);
 		},
 		[value, onFieldChange, highIsGood, legendDefinitions],
 	);
