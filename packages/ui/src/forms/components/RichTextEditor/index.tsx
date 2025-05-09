3be57ad4773e2ea01eae76452d21a1baaff619f7
Gift Nnko <nnkogift@gmail.com>
2025-05-09 09:08:13 +0300

    feat(rich-text-editor): upgrade `jodit-react` to v5.2.19 and enhance config customization
    Upgraded `jodit-react` from v1.3.39 to v5.2.19 to leverage the latest features and improvements. Updated the `RichTextEditor` component to allow for additional configuration overrides via the new optional `config` prop. These changes improve flexibility and maintain compatibility with newer dependencies.

--- packages/ui/src/forms/components/RichTextEditor/index.tsx
+++ packages/ui/src/forms/components/RichTextEditor/index.tsx
@@ -3,37 +3,54 @@ import JoditEditor, { Jodit } from "jodit-react";
 import React, { forwardRef } from "react";
 import { FieldProps } from "../../interfaces";
 
-export interface RichTextEditorProps extends FieldProps {}
+export interface RichTextEditorProps extends FieldProps {
+	config?: Parameters<typeof JoditEditor>[0]["config"];
+}
 
 export const RichTextEditor: React.FC<RichTextEditorProps> = forwardRef<
-	Jodit,
+	typeof Jodit,
 	RichTextEditorProps
->(({ name, label, value, onChange, error, warning, ...props }, ref) => {
-	const config = {
-		readonly: false,
-		defaultFontSizePoints: "pt",
-	};
-	return (
-		<Field
-			{...props}
-			name={name}
-			label={label}
-			error={Boolean(error)}
-			warning={Boolean(warning)}
-			validationText={
-				typeof props.warning === "string"
-					? props.warning
-					: typeof error === "string"
-						? error
-						: undefined
-			}
-		>
-			<JoditEditor
-				ref={ref}
-				value={value}
-				onBlur={(newValue: any) => onChange(newValue)}
-				config={config}
-			/>
-		</Field>
-	);
-});
+>(
+	(
+		{
+			name,
+			label,
+			value,
+			onChange,
+			error,
+			config: overrideConfig,
+			warning,
+			...props
+		},
+		ref,
+	) => {
+		const config: Parameters<typeof JoditEditor>[0]["config"] = {
+			readonly: false,
+			defaultFontSizePoints: "pt" as const,
+			...(overrideConfig ?? {}),
+		};
+		return (
+			<Field
+				{...props}
+				name={name}
+				label={label}
+				error={Boolean(error)}
+				warning={Boolean(warning)}
+				validationText={
+					typeof props.warning === "string"
+						? props.warning
+						: typeof error === "string"
+							? error
+							: undefined
+				}
+			>
+				<JoditEditor
+					ref={ref}
+					value={value}
+					onBlur={(newValue: any) => onChange(newValue)}
+					config={config}
+				/>
+			</Field>
+		);
+	},
+);
