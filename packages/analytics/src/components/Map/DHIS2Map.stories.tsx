80fafcc1c23c30427c24ac16ed59a33d9d019ebb
godlisten-ngatara <gpn0021@gmail.com>
2025-08-14 13:05:09 +0300

    feat(Map stories): Added a story for the time slider component

--- packages/analytics/src/components/Map/DHIS2Map.stories.tsx
+++ packages/analytics/src/components/Map/DHIS2Map.stories.tsx
@@ -42,17 +42,34 @@ Controls.args = {
 				sizeModes: ["A4Landscape", "A4Portrait", "Current"],
 			},
 		},
+	],
+};
+export const TimeLapse: Story = {
+	name: "Time lapse",
+};
+TimeLapse.args = {
+	orgUnitSelection: {
+		orgUnits: [],
+		userOrgUnit: true,
+		userSubUnit: true,
+		userSubX2Unit: true,
+	},
+	controls: [
+		
 		{
 			type: "temporalslider",
 			position: "bottomright",
+			 // Change every 1 second
 			options: {
 				hidden: false,
 				hideControlContainer: false,
+				slideInterval: 3000,
 			},
 		},
 	],
+	periodSelection: {
+		periods: ["202301", "202302", "202303", "202304", "202305", "202306", "202307", "202308", "202309", "202310", "202311", "202312"],}
 };
-
 export const BoundaryLayer: Story = {
 	name: "Boundary layer",
 };
