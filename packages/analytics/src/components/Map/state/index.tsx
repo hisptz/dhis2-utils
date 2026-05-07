8a29d0a5cac2d616f67b026151d479380144ca1a
Allen7702 <allen22052000@gmail.com>
2026-05-07 11:40:08 +0300

    fix:  update MapProvider context

--- packages/analytics/src/components/Map/state/index.tsx
+++ packages/analytics/src/components/Map/state/index.tsx
@@ -24,6 +24,7 @@ export const MapPeriodContext = createContext<
 	| {
 			periods?: BasePeriod[];
 			range?: { start: Date; end: Date };
+			renderingStrategy?: "SINGLE" | "TIMELINE";
 	  }
 	| undefined
 >({
